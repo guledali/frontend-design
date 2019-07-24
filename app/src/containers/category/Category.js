736d681b65e9019d211a94ad63810c61db5d1c6a
“guledali” <guled.ali01@gmail.com>
2019-07-24 18:02:46 +0200

    Making the data display in the page

--- app/src/containers/category/Category.js
+++ app/src/containers/category/Category.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
 import { getCategoryList } from './state/category';
 import './Category.scss';
+import { getProductImageLink } from '../../utils/imageUtils';
 
 export default class Category extends Component {
-  state = { category: {} };
+  state = { 
+    category: {
+      products: []
+    } 
+  };
+
   componentDidMount() {
     const loadedData = getCategoryList();
 
     this.setState({ category: loadedData });
   }
+  
+  
 
   render() {
+    console.log(this.state.category)
     /*
      * You can get the category data from the category state:
      * const { category } = this.state;
      */
+
+     const { category } = this.state
     return (
       <div className="hello-world">
-        Hello world!
-        <span role="img" aria-label="Wave">
-          👋🏻
-        </span>
+        <ul>
+          {category.products.map(item => (
+            <li key={item.id}>
+              <span>{item.name}</span>
+              <img src={getProductImageLink(item)} alt={item.name} />
+              <span>fr. {item.cheapestPrice.amount} kr</span>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
